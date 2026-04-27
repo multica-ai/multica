39bbd71901295bb9e5bdf409c977bf54be28333c
Bright Zheng <bzqzheng@gmail.com>
2026-04-27 19:29:10 -0400

    fix(server): include id and url in no-workspace file upload response

--- server/internal/handler/file.go
+++ server/internal/handler/file.go
@@ -240,8 +240,9 @@ func (h *Handler) UploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	writeJSON(w, http.StatusOK, map[string]string{
+		"id":       id.String(),
+		"url":      link,
 		"filename": header.Filename,
-		"link":     link,
 	})
 }
 
