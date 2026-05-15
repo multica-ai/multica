02165f6cbd75e61fa251e97e1accfe7d2b263d34
guodage <guodage003@gmail.com>
2026-05-15 18:43:11 +0800

    fix(cli): split Unix terminal functions out of cmd_run.go for Windows cross-compilation
    watchTerminalResize, makeStdinRaw, and isTerminal use Unix-specific
    syscalls (SIGWINCH, IoctlGetTermios, etc.) that are not available on
    Windows. Move them to cmd_run_unix.go and add Windows stubs.

--- server/cmd/multica/cmd_run.go
+++ server/cmd/multica/cmd_run.go
@@ -423,27 +423,6 @@ func localCLIProcessEnv(base []string, env localCLIEnv) []string {
 	return out
 }
 
-func watchTerminalResize(ptmx *os.File) func() {
-	_ = pty.InheritSize(os.Stdin, ptmx)
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGWINCH)
-	done := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case <-ch:
-				_ = pty.InheritSize(os.Stdin, ptmx)
-			case <-done:
-				return
-			}
-		}
-	}()
-	return func() {
-		signal.Stop(ch)
-		close(done)
-	}
-}
-
 func firstString(obj map[string]any, keys ...string) string {
 	for _, key := range keys {
 		if value := stringValue(obj[key]); value != "" {
@@ -451,35 +430,6 @@ func firstString(obj map[string]any, keys ...string) string {
 		}
 	}
 	return ""
-}
-
-func makeStdinRaw() (func(), error) {
-	fd := int(os.Stdin.Fd())
-	if !isTerminal(fd) {
-		return func() {}, nil
-	}
-	oldState, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
-	if err != nil {
-		return nil, fmt.Errorf("read terminal state: %w", err)
-	}
-	raw := *oldState
-	raw.Iflag &^= unix.BRKINT | unix.ICRNL | unix.INPCK | unix.ISTRIP | unix.IXON
-	raw.Oflag &^= unix.OPOST
-	raw.Cflag |= unix.CS8
-	raw.Lflag &^= unix.ECHO | unix.ICANON | unix.IEXTEN | unix.ISIG
-	raw.Cc[unix.VMIN] = 1
-	raw.Cc[unix.VTIME] = 0
-	if err := unix.IoctlSetTermios(fd, ioctlWriteTermios, &raw); err != nil {
-		return nil, fmt.Errorf("set terminal raw mode: %w", err)
-	}
-	return func() {
-		_ = unix.IoctlSetTermios(fd, ioctlWriteTermios, oldState)
-	}, nil
-}
-
-func isTerminal(fd int) bool {
-	_, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
-	return err == nil
 }
 
 func forwardSignals(proc *os.Process) func() {
