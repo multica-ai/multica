d0e8f29ff85cbfebede7a78ed0cfd03edb2e7b3f
airthor <airthor@163.com>
2026-05-27 14:37:31 +0800

    feat: 使用 config plugin作为原生资源生成源

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -17,6 +17,7 @@ export default {
     wsUrl: process.env.EXPO_PUBLIC_WS_URL || app.expo.extra.wsUrl,
   },
   plugins: [
+    "./plugins/with-multica-android-native.cjs",
     ...(app.expo.plugins || []),
     ...(googleIosUrlScheme
       ? [
---
+++ apps/mobile/plugins/android/FullscreenStatusBarPackage.kt
@@ -0,0 +1,17 @@
+package __PACKAGE_NAME__
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class FullscreenStatusBarPackage : ReactPackage {
+  @Suppress("OVERRIDE_DEPRECATION")
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(FullscreenStatusBarModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
