08af251a7a8745c41f542267c8d02d9ac0c5ce9e
Contributor <296671657@qq.com>
2026-05-30 14:48:10 +0800

    fix: export MentionRenderProps from markdown barrel

--- packages/ui/markdown/index.ts
+++ packages/ui/markdown/index.ts
@@ -1,4 +1,4 @@
-export { Markdown, MemoizedMarkdown, type MarkdownProps, type RenderMode } from './Markdown'
+export { Markdown, MemoizedMarkdown, type MarkdownProps, type RenderMode, type MentionRenderProps } from './Markdown'
 export { CodeBlock, InlineCode, type CodeBlockProps } from './CodeBlock'
 export { StreamingMarkdown, type StreamingMarkdownProps } from './StreamingMarkdown'
 export { preprocessLinks, detectLinks, hasLinks } from './linkify'
