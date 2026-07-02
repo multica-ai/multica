b7d54b939a0ae903b7e429056c62d82c63820102
luochunneng <luochunneng@qianshi.cn>
2026-07-02 17:43:20 +0800

    test(editor): pass suggestion signal in slash tests

--- packages/views/editor/extensions/slash-command-suggestion.test.tsx
+++ packages/views/editor/extensions/slash-command-suggestion.test.tsx
@@ -89,6 +89,7 @@ function items(qc: QueryClient, query = ""): SlashCommandItem[] {
   return config.items!({
     query,
     editor: {} as never,
+    signal: new AbortController().signal,
   }) as SlashCommandItem[];
 }
 
