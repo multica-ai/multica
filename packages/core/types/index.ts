eaa86a20ae26c48b09d36132386a959277fa9f5a
chouti <chouti@upai.com>
2026-07-09 09:43:33 +0800

    fix: export AdminUserWorkspace type + remove unused time import

--- packages/core/types/index.ts
+++ packages/core/types/index.ts
@@ -65,7 +65,7 @@ export type {
   IssueUsageSummary,
 } from "./agent";
 export { RUNTIME_PROFILE_PROTOCOL_FAMILIES } from "./agent";
-export type { Workspace, WorkspaceRepo, Member, MemberRole, User, MemberWithUser, Invitation, AdminUser, AdminWorkspaceSummary, AdminPendingInvitation } from "./workspace";
+export type { Workspace, WorkspaceRepo, Member, MemberRole, User, MemberWithUser, Invitation, AdminUser, AdminUserWorkspace, AdminWorkspaceSummary, AdminPendingInvitation } from "./workspace";
 export type { InboxItem, InboxSeverity, InboxItemType, InboxWorkspaceUnread } from "./inbox";
 export type { NotificationGroupKey, NotificationGroupValue, NotificationPreferences, NotificationPreferenceResponse } from "./notification-preference";
 export type { Comment, CommentType, CommentAuthorType, CommentTriggerPreview, CommentTriggerPreviewAgent, CommentTriggerSource, Reaction } from "./comment";
--- server/cmd/multica/cmd_workspace.go
+++ server/cmd/multica/cmd_workspace.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strings"
 	"text/tabwriter"
-	"time"
 	"unicode/utf8"
 
 	"github.com/spf13/cobra"
